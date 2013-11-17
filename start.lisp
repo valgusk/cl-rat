7009d9e6b2cb991835becd604f821f49c37d0ac3
Valery Guskov <valerijs.gusjkovs@gmail.com>
2013-11-17 21:02:47 +0200

    added correct wall creating code

--- start.lisp
+++ start.lisp
@@ -37,7 +37,7 @@
 (defkernel update-sight (float ((rot float) (x float) (y float) (x1 float) (y1 float)
                                 (where float*) (start-off int) (start-deg float)
                                 (end-deg float) (type float) (active float)))
-  (let* ((stp 0.005) ;fatness of object
+  (let* ((stp 0.005) ;fatness of object = 0.005 * 2 = 1/100 of field
          ;;get angles object covers
          (a (get-obj-angle rot x y (- x1 stp) (- y1 stp)))
          (b (get-obj-angle rot x y (- x1 stp) (+ y1 stp)))
@@ -113,6 +113,41 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;     main application code      ;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;wall block
+
+(defun make-walls (&optional (count 100) (len 50) (existing nil))
+  (labels
+    ((wall-def nil (mapcar #'random `(100 100 ,(* PI 2) ,len)))
+     (stick-p (a b) (> 2 (apply #'max (mapcar #'abs (mapcar '- a b)))))
+     (by-x (angle) (member (floor (/ angle PI 1/4)) '(0 3 4 7)))
+     (sticks (a) (lambda (bs) (find a bs :test #'stick-p)))
+     (make-grps (crosses grps &optional (i 0) (grp (nth i grps)))
+       (cond ((null crosses) grps)
+             ((null (car crosses)) (make-grps (cdr crosses) grps (1+ i)))
+             ((zerop grp) nil)
+             (t (make-grps (cdr crosses) (substitute 0 grp grps) (1+ i)))))
+     (unblock-wall (fixed w walls &optional grps ok-bricks)
+       (symbol-macrolet
+         ((new-grps (make-grps (mapcar (sticks (car w)) walls) grps))
+          (bad-p (when walls (not new-grps)))
+          (ok-grps (if (or fixed ok-bricks) (mapcar #'1+ (cons 0 grps)) grps))
+          (ok-walls (if ok-bricks (cons ok-bricks walls) walls)))
+         (cond (fixed (values (cons w walls) ok-grps))
+               ((null w) (values ok-walls ok-grps))
+               (bad-p (unblock-wall nil (cdr w) ok-walls ok-grps))
+               (t (unblock-wall nil (cdr w) walls new-grps (cons (car w) ok-bricks))))))
+     (make-bricks (x y angle size)
+       (loop for i below size
+             for x1 = x then (if (by-x angle) (1+ x1) (+ x (* i (cos angle))))
+             for y1 = y then (if (by-x angle) (+ y (* i (sin angle))) (1+ y1))
+             when (>= 99.4 (max x1 y1) (min x1 y1) -0.5)
+             collect (list (round x1) (round y1))))
+     (make-all-bricks (existing left)
+       (multiple-value-call #'unblock-wall (nth left existing)
+         (apply #'make-bricks (or (nth left existing) (wall-def)))
+         (and (plusp left) (make-all-bricks existing (1- left))))))
+    (make-all-bricks existing (+ count (list-length existing)))))
 
 
 (defstruct basement rats plants cats walls)
